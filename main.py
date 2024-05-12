32d8b3789bfc30bdaef0ba41f917c6d85f885001
james <james.yan.dev@gmail.com>
2024-05-12 09:21:10 +0100

    Removed the comment about future updates, as it has been added as issues in the repository.

--- main.py
+++ main.py
@@ -5,7 +5,3 @@ from gui import GUI
 price_scraper = PriceScraper()
 item_manager = ItemManager(price_scraper)
 gui = GUI(item_manager)
-
-# Future Updates - email alerts, input validation, ordered dictionary, scrolling
-
-
